e3147c88e039ebc8e2836b230ba4da103cb77836
Shizuu <coolcoledude111@gmail.com>
2022-10-06 13:32:59 +0100

    all

---
+++ script.js
@@ -0,0 +1,11 @@
+const container = document.querySelector('.container')
+const btnBlack = document.createElement('button')
+const btnGrey = document.createElement('button')
+const btnRGB = document.createElement('button')
+const btnSize = document.createElement('button')
+const buttonsContainer = document.querySelector('buttons')
+
+
+function createDivs(col, row){
+
+}
